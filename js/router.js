31044a21dd0eb4e5c9e7cef014e989a1ec69ddf2
Tiago Mota <tiagomota265@gmail.com>
2022-10-26 23:45:47 -0300

    first

---
+++ js/router.js
@@ -0,0 +1,27 @@
+export class Router {
+    routes = {}
+    add(routeName, page){
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.handle()
+    }
+    handle() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+
+        fetch(route)
+            .then((res) => res.text())
+            .then((html) => {
+                document.getElementById("app").innerHTML = html
+            })
+
+
+    }
+}
